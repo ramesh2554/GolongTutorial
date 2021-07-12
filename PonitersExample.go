fa4d05ca2f159f2532f0802096d2d274a108665f
ramesh2554 <konakalla.ramesh143@gmail.com>
2021-07-12 21:26:34 +0530

    even/odd using Slice

---
+++ PonitersExample.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*var p *int
+	// fmt.Println(*p) ---> error this values nothing stores in  var p
+	fmt.Println(&p)*/
+	// & -------> i want to hold a reference of things like address
+	// * -------> when we use * it means its a pointer
+	var lifebooster float64 = 99.2 // assign value
+	lifeboosterRef := &lifebooster // stores address , (we can't declare var = lifeboosterRef = &lifebooster
+
+	fmt.Println(lifebooster)     // prints value ----> 99.2
+	fmt.Println(lifeboosterRef)  // prints stored address
+	fmt.Println(*lifeboosterRef) // prints value because we use * for access value
+}
